6779cc1aadd6c82b7cb15762b27ebb7ca72da913
isabelpt <56853689+isabelpt@users.noreply.github.com>
2020-08-05 11:33:38 -0400

    First xcode proj

---
+++ StoryBoardIntro/StoryBoardIntro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryBoardIntro
+//
+//  Created by Isabel Prado-Tucker on 8/5/20.
+//  Copyright © 2020 Isabel Prado-Tucker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    print("testing testing!!!")
+    }
+
+}
+
